0b6ee38406fa622e284fef71d194844d1329c0d6
dev-iskan <successfuliskan@gmail.com>
2019-02-21 11:18:54 +0500

    added base route, controller, added fake data, installed  node_modules

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,12 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::group(['namespace' => 'Admin', 'prefix' => 'admin'], function () {
+    Route::get('/users', 'UserController@index');
+});
